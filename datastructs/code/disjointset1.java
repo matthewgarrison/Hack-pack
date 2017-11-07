efa0105dc9ac46b88d4c6ff8a5b5842ad86b1262
Matthew <matthewgarrison@knights.ucf.edu>
2017-11-07 04:31:35 -0500

    Add runtime and space complexities

--- datastructs/code/disjointset1.java
+++ datastructs/code/disjointset1.java
@@ -7,31 +7,21 @@ class DisjointSet {
 		for (int i = 0; i < numNodes; i++) parents[i] = i;
 	}
 	
-	int findRoot(int index) {
-		if (parents[index] != index) 
-			parents[index] = findRoot(parents[index]);
-        return parents[index];
+	int findRoot(int idx) {
+		if (parents[idx] != idx) parents[idx] = findRoot(parents[idx]);
+        return parents[idx];
     }
-	
+    
 	// Union by rank. Returns false if the union did not occur.
 	boolean union(int index1, int index2) {
 		int root1 = findRoot(index1);
 		int root2 = findRoot(index2);
 		if (root1 == root2) return false;
 		
-		/*
-		We put the tree with the lower rank directly under the root of the 
-		other tree. If they have the same rank, we arbitrarily decide to place 
-		the first under the second.
-		*/
-        if (ranks[root1] <= ranks[root2]) parents[root1] = root2;
-		else parents[root2] = root1;
-		/*
-		This is the only thing that differs from the case where the first 
-		tree's rank is less than the rank of the second tree.
-		*/
-		if (ranks[root1] == ranks[root2]) ranks[root2]++;
-
+		if (ranks[root1] < ranks[root2]) parents[root1] = root2;
+		else if (ranks[root1] > ranks[root2]) parents[root2] = root1;
+		else { parents[root1] = root2; ranks[root2]++; }
+        
         return true;
 	}
 }
