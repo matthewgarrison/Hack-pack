8a78c85bd8625efa5673826a02b9e958617388a5
Matthew <matthewgarrison@knights.ucf.edu>
2017-11-21 18:52:35 -0500

    Shorten Floyd-Warshall and move to Graphs section

--- algorithms/code/floydwarshall1.java
+++
@@ -1,6 +0,0 @@
-int[][] path = new int[n][n];
-for (int i = 0; i < n; i++)
-	for (int j = 0; j < n; j++) {
-		if (adj[i][j] == INFINITY) path[i][j] = -1;
-		else path[i][j] = i;
-	}
--- algorithms/code/floydwarshall3.java
+++ algorithms/code/floydwarshall3.java
@@ -1,8 +1,6 @@
 ArrayList<Integer> indexes = new ArrayList<Integer>();
 indexes.add(end);
-// Loop through each previous vertex until you get back to start.
 while (path[start][end] != start) {
-	indexes.add(path[start][end]);
-	end = path[start][end];
+	indexes.add(path[start][end]); 	end = path[start][end];
 }
 indexes.add(start);
